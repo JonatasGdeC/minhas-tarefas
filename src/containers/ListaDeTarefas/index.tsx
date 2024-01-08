7ca9ba26c6e890b9239d32dfe847afd1805bdff4
Jonatas <jonatas.07022003@gmail.com>
2024-01-08 16:04:17 -0300

    Aplicando o Redux

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,28 +1,12 @@
+import { useSelector } from 'react-redux'
+
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
-
-const tarefas = [
-  {
-    titulo: 'Estudar TypeScript',
-    descricao: 'Ver a aula 3 da EBAC',
-    prioridade: 'Importante',
-    status: 'Pendente'
-  },
-  {
-    titulo: 'Pagar a conta de internet',
-    descricao: 'Baixar Fatura no Gmail',
-    prioridade: 'Urgente',
-    status: 'Concluída'
-  },
-  {
-    titulo: 'Ir para a academia',
-    descricao: 'Fazer treino B',
-    prioridade: 'Importante',
-    status: 'Pendente'
-  }
-]
+import { RootReducer } from '../../store'
 
 const ListaDeTarefas = () => {
+  const { tarefas } = useSelector((state: RootReducer) => state)
+
   return (
     <Container>
       <p>Tarefas marcadas como: &quot;Categoria&ldquo; e &quot;Termo&ldquo;</p>
